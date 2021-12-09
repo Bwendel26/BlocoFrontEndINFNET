c4bbf88d70a1090e4597a4272fba03fd64251fd0
Bwendel26 <bwendel26999@gmail.com>
2021-12-08 23:22:44 -0300

    Finalizando AT de JS

---
+++ javascript/TP3/q2/q2.js
@@ -0,0 +1,9 @@
+/*Crie um componente que tenha uma caixa de texto que só aceita números e tenha o rótulo
+(label) Calcular o fatorial de. O componente deve ainda possuir um botão calcular que 
+quando acionado deve calcular o fatorial do número digitado, imprimido o resultado e 
+o tempo necessário para a execução. (Dica: usar o objeto javascript Date)
+
+    O resultado deve ser mostrado no formato 53! = XXXXXX (yyy milisegundos);
+    O código deve calcular o fatorial e não apenas imprimir uma string com a resposta.
+*/
+
