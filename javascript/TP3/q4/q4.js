5a6a894e261c5da5634d247ab7708a1d50b75bff
Bwendel26 <bwendel26999@gmail.com>
2021-12-10 04:51:47 -0300

    Adiciona arquivos e mods referentes a q4 do TP3 de JS (funcional)

--- javascript/TP3/q4/q4.js
+++ javascript/TP3/q4/q4.js
@@ -7,3 +7,39 @@ Os números devem estar entre valor mínimo e valor máximo (inclusives, ou
 seja incluindo o valor mínimo e valor máximo). Lembre-se que para calcular 
 quantidade de números deve ser menor que a soma de valor mínimo e valor máximo.
 */
+
+function criarArrayValoresAleatorios() {
+
+    qtd = parseInt(document.getElementById("qtdN").value);
+    max = parseInt(document.getElementById("valMax").value);
+    min = parseInt(document.getElementById("valMin").value);
+    let valores = [];
+
+    for (let i = 0; i < qtd; i++) {
+        let condicao = true;
+        numRandom = Math.floor(Math.random() * (max - min + 1) + min);
+        for (let j = 0; j < qtd; j++) {
+            if(valores[j] == numRandom) {
+                condicao = false;
+            }
+        }
+        if(condicao) {
+            valores.push(numRandom);
+        }
+    }
+
+    return valores;
+}
+
+function ordenaValores() {
+
+    document.getElementById("form4").addEventListener('submit', e => {
+        e.preventDefault();
+    });
+
+    let arrayValores = criarArrayValoresAleatorios();
+    arrayOrdenado = `<p>${arrayValores.sort()}</p>`;
+
+    console.log(arrayOrdenado);
+    return document.getElementById("resultadoQ4").innerHTML = arrayOrdenado;
+}
